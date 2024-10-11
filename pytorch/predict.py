fixed prediction without thermal pe, added normalization to hyperparamter file

pytorch/predict.py
@@ -27,9 +27,11 @@ from rasterio.warp import reproject, Resampling
 from rasterio.mask import mask
 
 def predict(args_predict):
-
     preprocess_params = load_preprocess_settings(os.path.dirname(args_predict["model_path"]))
     preprocess_params["aois"] = args_predict["aois"]
+
+    assert (preprocess_params["thermal_time"] is None and args_predict["thermal_time_prediction"] is None) or \
+           (preprocess_params["thermal_time"] is not None and args_predict["thermal_time_prediction"] is not None), "Different Positional Encoding used for Training and Prediction"
 
     if args_predict["years"] == None:
         preprocess_params["years"] = [int(re.search(r'(\d{4})', os.path.basename(f)).group(1)) for f in preprocess_params["aois"] if re.search(r'(\d{4})', os.path.basename(f))]
@@ -246,19 +248,26 @@ def predict_singlegrid(model, tiles, args_predict):
     month = int(args_predict['time_range'][1].split('-')[0])
     day = int(args_predict['time_range'][1].split('-')[1])
     thermal_dataset = args_predict["thermal_time_prediction"]
-    data, doy_single, thermal = read_tif_files(tiles, order, year, month, day, thermal_dataset)
-    # Stack the bands and perform initial reshape in NumPy
-    data_thermal = np.stack(thermal, axis=0)
-    data = np.stack(data, axis=1)
-    # Reshape data for PyTorch [sequence length, number of bands, height, width]
-    seq_len, num_bands, height, width = data.shape
-    XY = height * width
-    data_thermal = data_thermal.reshape(seq_len, XY)
+    if thermal_dataset is not None:
+        data, doy_single, thermal = read_tif_files(tiles, order, year, month, day, thermal_dataset)
+        data_thermal = np.stack(thermal, axis=0)
+        data = np.stack(data, axis=1)
+        # Reshape data for PyTorch [sequence length, number of bands, height, width]
+        seq_len, num_bands, height, width = data.shape
+        XY = height * width
+        data_thermal = data_thermal.reshape(seq_len, XY)
+        data_thermal = np.transpose(data_thermal, (1, 0))
+    else:
+        # Stack the bands and perform initial reshape in NumPy
+        data, doy_single, thermal = read_tif_files(tiles, order, year, month, day, thermal_dataset)
+        data = np.stack(data, axis=1)
+        # Reshape data for PyTorch [sequence length, number of bands, height, width]
+        seq_len, num_bands, height, width = data.shape
+        XY = height * width
     data = data.reshape(seq_len, num_bands, XY)
-
     # Reorder dimensions for PyTorch [XY, number of bands, sequence length] using NumPy
-    data_thermal = np.transpose(data_thermal, (1, 0))
     data = np.transpose(data, (2, 1, 0))
+
     # Move model to the appropriate device
     device = next(model.parameters()).device
 
@@ -267,7 +276,8 @@ def predict_singlegrid(model, tiles, args_predict):
     with torch.no_grad():
         for i in tqdm(range(0, data.shape[0], chunksize)):
             batch = data[i:i + chunksize] * normalizing_factor
-            batch_thermal = data_thermal[i:i + chunksize]
+            if thermal_dataset is not None:
+                batch_thermal = data_thermal[i:i + chunksize]
             non_zero_mask = np.any(batch != 0, axis=(1, 2))
             doy = np.array(doy_single)
             doy = np.tile(doy, (batch.shape[0], 1))
@@ -277,13 +287,14 @@ def predict_singlegrid(model, tiles, args_predict):
             else:
                 batch_non_zero = batch[non_zero_mask]
                 doy_non_zero = doy[non_zero_mask]
-                batch_thermal = batch_thermal[non_zero_mask]
-
                 batch_tensor = torch.tensor(batch_non_zero, dtype=torch.float32, device=device)
                 doy_tensor = torch.tensor(doy_non_zero, dtype=torch.long, device=device)
-                thermal_tensor = torch.tensor(batch_thermal, dtype=torch.long, device=device)
-
-                predictions_non_zero = model(batch_tensor, doy_tensor,thermal_tensor)[0]
+                if thermal_dataset is not None:
+                    batch_thermal = batch_thermal[non_zero_mask]
+                    thermal_tensor = torch.tensor(batch_thermal, dtype=torch.long, device=device)
+                    predictions_non_zero = model(batch_tensor, doy_tensor, thermal_tensor)[0]
+                else:
+                    predictions_non_zero = model(batch_tensor, doy_tensor,thermal_tensor = None)[0]
 
                 # Handle normalization response factor
                 norm_factor_response = args_predict.get("norm_factor_response")
@@ -375,7 +386,7 @@ def predict_raster(args_predict):
 
     hyp = load_hyperparametersplus(os.path.dirname(args_predict["model_path"]))
     args_predict.update(hyp)
-    if args_predict["thermal_time_prediction"] != None and args_predict["thermal_time"] != None:
+    if args_predict["thermal_time_prediction"] is not None and args_predict["thermal_time"] is not None:
         print("Applying Transformer Model with Thermal Positional Encoding!")
     args_predict['store'] = os.path.dirname(args_predict['model_path'])
     # create hw_monitor output dir if it doesn't exist

pytorch/utils/trainer.py
@@ -34,8 +34,10 @@ class Trainer():
                  optimizer=None,
                  logger=None,
                  response = None,
+                 norm_factor_response = None,
                  **kwargs):
 
+        self.norm_factor_response = norm_factor_response
         self.response = response
         self.epochs = epochs
         self.batch_size = validdataloader.batch_size
@@ -200,13 +202,13 @@ class Trainer():
                 logprobabilities, deltas, pts, budget = self.model.forward(inputs.transpose(1, 2), doy)
             else:
                 logprobabilities, deltas, pts, budget = self.model.forward(inputs.transpose(1,2))
-
             #print(inputs.transpose(1,2))
             #print(self.model(inputs.transpose(1,2))[0])
             if self.response == "classification":
                 loss = F.nll_loss(logprobabilities, targets)
             else:
                 loss = F.mse_loss(logprobabilities.squeeze(1),targets)
+
 
             loss.backward()
 
@@ -235,7 +237,17 @@ class Trainer():
             else:
                 stats = metric.add(stats)
                 # Calculate the MSE and R2 for this iteration
-                rmse_r2_stats = metric.update_mat(targets.detach().cpu().numpy(),logprobabilities.squeeze(1).detach().cpu().numpy())
+                targets = targets.detach().cpu().numpy()
+                responses = logprobabilities.squeeze(1).detach().cpu().numpy()
+
+                if self.norm_factor_response == "log10":
+                    targets = np.power(10, targets) - 1
+                    responses = np.power(10, responses) - 1
+                elif self.norm_factor_response is not None and self.norm_factor_response != 0:
+                    targets /= self.norm_factor_response
+                    responses /= self.norm_factor_response
+
+                rmse_r2_stats = metric.update_mat(targets,responses)
 
                 # Add MSE and R2 to the `stats` dictionary
                 stats["r2"] = rmse_r2_stats["r2"]
@@ -302,7 +314,17 @@ class Trainer():
                 else:
                     stats = metric.add(stats)
                     # Calculate the MSE and R2 for this iteration
-                    rmse_r2_stats = metric.update_mat(targets.detach().cpu().numpy(),logprobabilities.squeeze(1).detach().cpu().numpy())
+                    targets_vali = targets.detach().cpu().numpy()
+                    responses_vali = logprobabilities.squeeze(1).detach().cpu().numpy()
+
+                    if self.norm_factor_response == "log10":
+                        targets_vali = np.power(10, targets_vali) - 1
+                        responses_vali = np.power(10, responses_vali) - 1
+                    elif self.norm_factor_response is not None and self.norm_factor_response != 0:
+                        targets_vali /= self.norm_factor_response
+                        responses_vali /= self.norm_factor_response
+
+                    rmse_r2_stats = metric.update_mat(targets_vali,responses_vali)
                     # Add MSE and R2 to the `stats` dictionary
                     stats["r2"] = rmse_r2_stats["r2"]
                     stats["rmse"] = rmse_r2_stats["rmse"]