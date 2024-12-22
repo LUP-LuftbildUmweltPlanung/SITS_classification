b538fa815a216072548b07bad79490364d70c4c5
benjaminstoeckigt <benjamin.stoeckigt@gmail.com>
2024-12-22 12:33:46 +0000

    added multimodel prediction, tile based inference with cleanup

--- class_main_3_predict.py
+++ class_main_3_predict.py
@@ -13,23 +13,25 @@ from pytorch.predict import predict
 
 
 args_predict = {
-    'project_name': "multi_tile",
+    'project_name': "tcd_v2_devtest_double_dingodiffheight",
     "process_folder": "/uge_mount/FORCE/new_struc/process/",
-    'model_path': '/uge_mount/FORCE/new_struc/process/results/_SITSModels/class_seal_3years/transformer/model_e120.pth', # Path to Model
-    'aois': glob.glob("/uge_mount/FORCE/new_struc/process/data/sealed/test_multitile_2023.shp"), # aois can be path or list. Path for Force Tile folder or list // process structure and shapefiles must be correct
+    'model_path': '/uge_mount/FORCE/new_struc/process/results/_SITSModels/class_cc_thermal_3y/transformer/model_e88.pth', # Path to Model
+    'model_path2': '/uge_mount/FORCE/new_struc/process/results/_SITSModels/class_vh_thermal_3y/transformer_respNone/model_e110.pth', # Path to Model,
+    'aois': glob.glob("/uge_mount/FORCE/new_struc/process/data/devtest_veg_aois/dingo_dif*.shp"), # aois can be path or list. Path for Force Tile folder or list // process structure and shapefiles must be correct
     #'aois': None,
     'years': None, ###Oberservation Year (last year of the timeseries), that should be defined for every AOI Shapefile - if "None" Years will be extracted from aoi FileName YYYY
-    #'reference_folder' : '/uge_mount/FORCE/new_struc/process/results/_SITSrefdata/class_vv_final/',
-    'reference_folder' : None, #"/uge_mount/FORCE/new_struc/process/results/_SITSrefdata/class_vh_thermal_3y_2022DElo_JoThermalJoAug/",#None, #Set Path if you want to predict the Test CSV File /path/to/_SITSrefdata/projectname
+    'reference_folder' : None,
+    #'reference_folder' : "/uge_mount/FORCE/new_struc/process/results/_SITSrefdata/class_vh_thermal_3y_2022DElo_JoThermalJoAug/",#None, #Set Path if you want to predict the Test CSV File /path/to/_SITSrefdata/projectname
     'probability' : False, # just gets recognized if classification
     'chunksize': 6000,  # 5years ts -> 2000
     'tmp_cleanup': True,
-    'thermal_time_prediction': None,#"/uge_mount/FORCE/new_struc/process/data/gdd/concatenated_gdd_start2015_3035.tif", #set None if not using
+    'thermal_time_prediction': "/uge_mount/FORCE/new_struc/process/data/gdd/concatenated_gdd_start2015_3035.tif", #set None if not using
     'force_dir': "/force",
     }
 
 if __name__ == '__main__':
     predict(args_predict)
+
 
 
 
